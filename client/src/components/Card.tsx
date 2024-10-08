225616effeee5aa91f529965887cdbd3558b550e
Janosch Püschel <janosch.pueschel@gmail.com>
2024-10-08 21:20:13 +0200

    refactor: update card to have optional className prop

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
-  className: string;
+  className?: string;
 }
 
 export default function Card({ children, className }: Props) {
