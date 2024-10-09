d8999df660d16b9e7bb40913a294be6b70ccd18f
Janosch Püschel <janosch.pueschel@gmail.com>
2024-10-09 22:55:11 +0200

    refactor: lifting up state of todos to parent component

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -1,13 +1,49 @@
+import { useState } from "react";
 import NewTodoItem from "./NewTodoItem";
 import Subheading from "./Subheading";
 import TodoItem from "./TodoItem";
 
+interface Todos {
+  id: number;
+  text: string;
+  done: boolean;
+}
+
 export default function TodoList() {
+  const [todos, setTodos] = useState<Todos[]>([
+    { id: 1, text: "Rubber cheese camembert de normandie gouda", done: false },
+    {
+      id: 2,
+      text: "Dolcelatte cheesecake paneer halloumi jarlsberg bavarian",
+      done: false,
+    },
+    { id: 3, text: "bergkase who moved", done: true },
+    { id: 4, text: "Cheesecake camembert de normandie", done: false },
+    { id: 5, text: "Smelly cheese cheese slices cheddar", done: true },
+  ]);
+
+  const toggleTodoDone = (id: number) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
+
   return (
     <>
       <Subheading title="Today's Tasks" />
       <div className="space-y-3">
-        <TodoItem />
+        {todos.map(({ id, text, done }) => (
+          <TodoItem
+            key={id}
+            id={id}
+            text={text}
+            done={done}
+            toggleTodoDone={toggleTodoDone}
+          />
+        ))}
+
         <NewTodoItem />
       </div>
     </>
