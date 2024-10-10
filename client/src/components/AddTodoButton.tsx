ca45e5cdc16fc5f61021f40204108f37c50a5bfb
Janosch Püschel <janosch.pueschel@gmail.com>
2024-10-10 16:49:16 +0200

    refactor: add prop to declare button type

--- client/src/components/AddTodoButton.tsx
+++ client/src/components/AddTodoButton.tsx
@@ -4,13 +4,15 @@ import { FaPlus } from "react-icons/fa6";
 interface Props {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
   className?: string;
+  type: "button" | "submit";
 }
 
-const AddTodoButton: FC<Props> = ({ onClick, className }) => {
+const AddTodoButton: FC<Props> = ({ onClick, className, type }) => {
   return (
     <button
       className={`${className} bg-blue-600  text-white rounded-full aspect-square`}
       onClick={onClick}
+      type={type}
     >
       <FaPlus className="mx-auto my-auto" />
     </button>
